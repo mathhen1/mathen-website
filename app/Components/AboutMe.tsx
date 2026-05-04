a21937df7aea2a68a54f330ff45cc1cb7d0f0b29
mathen <math.z.mhj@gmail.com>
2026-05-04 00:13:19 -0300

    fix: animation on scroll

--- app/Components/AboutMe.tsx
+++ app/Components/AboutMe.tsx
@@ -8,40 +8,45 @@ const AboutMe = () => {
     const [animate2, setAnimate2] = useState<string>("")
 
     useEffect(() => {
-        setAnimate2("animate-slide-in-right")
-        setTimeout(() => setAnimate("animate-slide-in-top"), 100)
-    }, [animate2, animate])
+        setAnimate("animate-slide-in-right")
+        setTimeout(() => setAnimate2("animate-slide-in-top"), 300)
+
+    }, [])
 
     useEffect(() => {
+
         window.addEventListener("scroll", () => {
             if (window.scrollY > 900) {
-                setAnimate2("")
-                setAnimate("")
+                setAnimate2(" ")
+                setAnimate(" ")
+            }
+
+            if (window.scrollY < 500) {
+                setAnimate("animate-slide-in-right")
+                setTimeout(() => setAnimate2("animate-slide-in-top"), 300)
             }
         })
-    }, [animate, animate2])
+    }, [animate2, animate])
 
-    if (animate === "") {
+    if (animate.includes(" ")) {
         return <div className="h-screen w-2/3"></div>
     }
 
-    if (animate2 === "") {
-        return (
-            <div className={"jane flex items-center justify-center h-screen w-2/3 border-2 border-violet-500 rounded-2xl divide-x-3 shadow-xl/30 shadow-sky-500 bg-gray-950 font-mono " + animate2}></div>
-        )
+    if (animate2.includes(" ")) {
+        return <div className={"h-screen w-2/3 " + animate}></div>
     }
 
     return (
-        <div className={"flex items-center justify-center h-screen w-2/3 border-2 border-violet-500 rounded-2xl divide-x-3 shadow-xl/30 shadow-sky-500 bg-gray-950 font-mono " + animate2}>
+        <div className={"flex items-center justify-center h-screen w-2/3 border-2 border-violet-500 rounded-2xl divide-x-3 shadow-xl/30 shadow-sky-500 bg-gray-950 font-mono " + animate}>
 
-            <div className={"flex flex-1 justify-center " + animate}>
+            <div className={"flex flex-1 justify-center " + animate2}>
                 <img src={"eu-foto.jpg"}
                     width={300}
                     height={300}
                     className="border rounded-3xl"></img>
             </div>
 
-            <div className={"flex flex-1 justify-center " + animate}>
+            <div className={"flex flex-1 justify-center " + animate2}>
                 <div className="flex flex-col">
                     <h1>Graduado em Analise e Desenvolvimento de Sistemas <br /> (Faculdade Aeso Barros Melo)</h1>
                     <br></br>
