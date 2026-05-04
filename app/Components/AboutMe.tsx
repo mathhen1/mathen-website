ea35e3703791bf41b59950ce52e9583b5f715ff6
mathen <math.z.mhj@gmail.com>
2026-05-03 21:56:30 -0300

    feat: animation on scroll in desktop

--- app/Components/AboutMe.tsx
+++ app/Components/AboutMe.tsx
@@ -4,24 +4,52 @@ import { useEffect, useState } from "react"
 
 const AboutMe = () => {
 
-    return (
-        <div className="flex items-center justify-center h-screen w-2/3 border-2 border-violet-500 rounded-2xl divide-x-3 shadow-xl/30 shadow-sky-500 bg-gray-950 
-        font-mono animate-slide-in-right">
+    const [animate, setAnimate] = useState<string>("")
+    const [animate2, setAnimate2] = useState<string>("")
 
-            <div className="flex flex-1 justify-center">
+    useEffect(() => {
+        setAnimate2("animate-slide-in-right")
+        setTimeout(() => {
+            setAnimate("animate-slide-in-top")
+        }, 500)
+    }, [animate, animate2])
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 900) {
+                setAnimate("")
+                setAnimate2("")
+            }
+        })
+    }, [])
+
+    if (animate === "" && animate === "") {
+        return <div className="h-screen w-2/3"></div>
+    }
+
+    if (animate === "") {
+        return (
+            <div className={"jane flex items-center justify-center h-screen w-2/3 border-2 border-violet-500 rounded-2xl divide-x-3 shadow-xl/30 shadow-sky-500 bg-gray-950 font-mono " + animate2}></div>
+        )
+    }
+
+    return (
+        <div className={"flex items-center justify-center h-screen w-2/3 border-2 border-violet-500 rounded-2xl divide-x-3 shadow-xl/30 shadow-sky-500 bg-gray-950 font-mono " + animate2}>
+
+            <div className={"flex flex-1 justify-center " + animate}>
                 <img src={"eu-foto.jpg"}
                     width={300}
                     height={300}
                     className="border rounded-3xl"></img>
             </div>
 
-            <div className="flex flex-1 justify-center">
+            <div className={"flex flex-1 justify-center " + animate}>
                 <div className="flex flex-col">
-                    <h1>Graduado em Analise e Desenvolvimento de Sistemas <br></br> (Aeso Barros Melo)</h1>
+                    <h1>Graduado em Analise e Desenvolvimento de Sistemas <br /> (Faculdade Aeso Barros Melo)</h1>
                     <br></br>
-                    <h1>Especializando em Engenharia de Software <br></br> (Centro Universitário Frassinetti do Recife)</h1>
+                    <h1>Especializando em Engenharia de Software <br /> (Centro Universitário Frassinetti do Recife)</h1>
                     <br></br>
-                    <h1>Inglês (Intermediário) - Leitura, escrita e entendimento</h1>
+                    <h1>Inglês - Nível Intermediário (Leitura, escrita e entendimento) <br /> (Wise Up Online) </h1>
                 </div>
             </div>
         </div>
