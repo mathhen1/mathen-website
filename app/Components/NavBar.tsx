8e72bfdcaa1a83009f7bfd6419f9525e9d990687
mathen <math.z.mhj@gmail.com>
2026-05-06 20:58:09 -0300

    fix: responsive navbar

--- app/Components/NavBar.tsx
+++ app/Components/NavBar.tsx
@@ -23,15 +23,22 @@ const NavBar = () => {
         )
     }
 
+    const handleTitle = () => {
+        window.location.reload()
+    }
+
     return (
-        <div className="flex flex-col font-sans w-full text-violet-500 bg-gray-800 border-b-2">
+        <div className="flex flex-col font-sans w-full text-violet-500 bg-gray-800 border-b-2 overflow-hidden">
 
-            <div id="navBar" className="flex flex-row font-mono p-5 animate-opacity">
+            <div id="navBar" className="flex flex-row font-mono md:h-full h-[10vh] p-5 animate-opacity">
 
-                <div className="flex-1 text-center p-1 text-4xl
+                <div className="flex p-1 md:text-2xl text-xl
                 md:text-left md:flex-none">
-                    <h1 className="font-extrabold bg-gradient-to-r from-slate-300 to-purple-500 text-transparent bg-clip-text">
-                        Matheus Henrique
+
+                    <h1 className="flex font-extrabold bg-gradient-to-r from-slate-300 to-purple-500 text-transparent bg-clip-text">
+                        <button onClick={handleTitle} className="hover:cursor-pointer">
+                            Matheus Henrique
+                        </button>
                     </h1>
                 </div>
 
@@ -44,10 +51,10 @@ const NavBar = () => {
 
                 {/* navbar on desktop */}
 
-                <div className="hidden md:flex md:gap-5 md:flex-1 md:justify-center md:items-center md:text-2xl">
+                <div className="hidden md:flex md:flex-1 md:flex-row md:gap-5 md:justify-center md:items-center md:text-lg md:w-full lg:text-2xl">
 
-                    <div className="md:text-center md:flex md:flex-row md:gap-8 md:flex-1 md:justify-center">
 
+                    <div className="flex flex-row w-full justify-center gap-5">
                         <a href="#about-me" className="hover:underline hover:decoration-sky-500 hover:underline-offset-8">
                             About Me
                         </a>
@@ -63,10 +70,9 @@ const NavBar = () => {
                         <a href="#contact" className="hover:underline hover:decoration-sky-500 hover:underline-offset-8">
                             Contact
                         </a>
-
                     </div>
 
-                    <div className="md:flex md:flex-none md:justify-end md:text-center md:gap-2 pr-2">
+                    <div className="flex flex-row gap-4">
                         <a href="https://www.linkedin.com/in/matheus-henrique-609083271/" className="hover:underline hover:decoration-sky-500 hover:underline-offset-8">
                             <p className="font-extrabold bg-gradient-to-r from-slate-300 to-purple-500 text-transparent bg-clip-text">
                                 Linkedin
@@ -79,6 +85,7 @@ const NavBar = () => {
                             </p>
                         </a>
                     </div>
+
                 </div>
 
             </div>
@@ -87,7 +94,7 @@ const NavBar = () => {
 
             <div className={"md:hidden flex-col justify-center gap-1 p-5 text-2xl " + nav}>
 
-                <div className="md:text-center flex flex-col text-center border rounded-lg">
+                <div className="md:text-center flex flex-col text-center border-b-2 rounded-lg">
                     <a href="#about-me" className="hover:underline hover:decoration-cyan-200 hover:underline-offset-8">
                         about me
                     </a>
